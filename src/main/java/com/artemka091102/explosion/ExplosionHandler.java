5d5d6a5ac070048a5993eb12b59b5921c3fb73bd
Artemka091102 <Ponomarev.Artem02@gmail.com>
2020-04-26 22:08:48 +0300

    done practically all

--- src/main/java/com/artemka091102/explosion/ExplosionHandler.java
+++ src/main/java/com/artemka091102/explosion/ExplosionHandler.java
@@ -15,27 +15,27 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 public class ExplosionHandler {
     @SubscribeEvent
     public static void explosion(ExplosionEvent event) {
-        World world = event.getWorld();
-        if (world.isRemote) return;
-
         Explosion explosion = event.getExplosion();
         List<BlockPos> blocksPos = explosion.getAffectedBlockPositions();
         Vec3d explosionPos = explosion.getPosition();
-        BlockPos newBlockPos;
-        Block newBlock;
-
+        World world = event.getWorld();
+    	BlockPos newBlockPos;
+    	Block newBlock;
         for (BlockPos blockPos : blocksPos) {
             for (int i = 0; i < 6; i++) {
-                newBlockPos = Utils.blockPosNearby(blockPos, i);
-                newBlock = world.getBlockState(newBlockPos).getBlock();
+            	newBlockPos = Utils.blockPosNearby(blockPos, i);
+            	newBlock = world.getBlockState(newBlockPos).getBlock();
                 if (!Utils.canBeExploded(newBlock)) continue;
-                if (!Utils.canPassThrough(world.getBlockState(Utils.blockPosNearby(newBlockPos, i)).getBlock()))
-                    continue;
-                Utils.throwBlock(world, newBlockPos,
-                        Utils.motion(Utils.centerOfBlock(newBlockPos), Utils.roundVec3d(explosionPos, 1),
-                                1 / (newBlock.getDefaultState().getHarvestLevel() + 2)));
+            	if (Utils.canPassThrough(world.getBlockState(Utils.blockPosNearby(newBlockPos, i)).getBlock())) {
+            		Utils.degradeBlock(world, newBlockPos, 1);
+            		Utils.throwBlock(world, newBlockPos,
+            				Utils.motion(Utils.centerOfBlock(newBlockPos), Utils.roundVec3d(explosionPos, 1),
+            						1 / (world.getBlockState(newBlockPos).getHarvestLevel()+2)));
+            	} else {
+            		Utils.degradeBlock(world, newBlockPos, 0.5F);
+            	}
             }
-            newBlock = world.getBlockState(blockPos).getBlock();
+        	newBlock = world.getBlockState(blockPos).getBlock();
             if (!Utils.canBeExploded(newBlock)) continue;
             Utils.degradeBlock(world, blockPos, 1);
         }
