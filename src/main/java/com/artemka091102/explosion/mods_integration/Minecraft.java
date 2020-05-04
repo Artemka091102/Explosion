b6b7119202daf540d43513e9365d171b50c78041
Roman <roma-svistunov@tut.by>
2020-05-04 19:45:06 +0300

    current support splitted

---
+++ src/main/java/com/artemka091102/explosion/mods_integration/Minecraft.java
@@ -0,0 +1,70 @@
+package com.artemka091102.explosion.mods_integration;
+
+import com.artemka091102.explosion.CrackedDict;
+
+import static com.artemka091102.explosion.CrackedDict.putToDict;
+
+public class Minecraft {
+    public static void SetupCrackedDict() {
+        putToDict("minecraft:grass_block", "minecraft:dirt");
+        putToDict("minecraft:cracked_stone_bricks", "minecraft:cobblestone");
+        putToDict("minecraft:infested_cracked_stone_bricks", "minecraft:infested_cobblestone");
+        putToDict("minecraft:cobblestone", "minecraft:gravel");
+        putToDict("minecraft:chiseled_sandstone", "minecraft:sandstone");
+        putToDict("minecraft:chiseled_red_sandstone", "minecraft:red_sandstone");
+        putToDict("minecraft:chiseled_stone_bricks", "minecraft:stone_bricks");
+        putToDict("minecraft:infested_chiseled_stone_bricks", "minecraft:infested_stone_bricks");
+        putToDict("minecraft:chiseled_quartz_block", "minecraft:quartz_block");
+        putToDict("minecraft:polished_granite", "minecraft:granite");
+        putToDict("minecraft:polished_diorite", "minecraft:diorite");
+        putToDict("minecraft:polished_andesite", "minecraft:andesite");
+        putToDict("minecraft:polished_granite_stairs", "minecraft:granite_stairs");
+        putToDict("minecraft:polished_diorite_stairs", "minecraft:diorite_stairs");
+        putToDict("minecraft:polished_andesite_stairs", "minecraft:andesite_stairs");
+        putToDict("minecraft:polished_granite_slab", "minecraft:granite_slab");
+        putToDict("minecraft:polished_diorite_slab", "minecraft:diorite_slab");
+        putToDict("minecraft:polished_andesite_slab", "minecraft:andesite_slab");
+        putToDict("minecraft:smooth_stone", "minecraft:stone");
+        putToDict("minecraft:smooth_red_sandstone", "minecraft:red_sandstone");
+        putToDict("minecraft:smooth_stone_slab", "minecraft:stone_slab");
+        putToDict("minecraft:smooth_red_sandstone_stairs", "minecraft:red_sandstone_stairs");
+        putToDict("minecraft:smooth_sandstone_stairs", "minecraft:sandstone_stairs");
+        putToDict("minecraft:smooth_red_sandstone_slab", "minecraft:red_sandstone_slab");
+        putToDict("minecraft:smooth_sandstone_slab", "minecraft:sandstone_slab");
+        putToDict("minecraft:sandstone", "minecraft:sand");
+        putToDict("minecraft:red_sandstone", "minecraft:red_sand");
+        putToDict("minecraft:smooth_sandstone", "minecraft:sandstone");
+        putToDict("minecraft:sandstone_slab", "minecraft:sand_slab");
+        putToDict("minecraft:cut_sandstone_slab", "minecraft:cut_sand_slab");
+        putToDict("minecraft:sandstone_stairs", "minecraft:sand_stairs");
+        putToDict("minecraft:red_sandstone_wall", "minecraft:red_sand_wall");
+        putToDict("minecraft:sandstone_wall", "minecraft:sand_wall");
+        putToDict("minecraft:bricks", "minecraft:cracked_bricks");
+        putToDict("minecraft:stone_bricks", "minecraft:cracked_stone_bricks");
+        putToDict("minecraft:infested_stone_bricks", "minecraft:infested_cracked_stone_bricks");
+        putToDict("minecraft:nether_bricks", "minecraft:cracked_nether_bricks");
+        putToDict("minecraft:prismarine_bricks", "minecraft:cracked_prismarine_bricks");
+        putToDict("minecraft:end_stone_bricks", "minecraft:end_stone");
+        putToDict("minecraft:red_nether_bricks", "minecraft:cracked_red_nether_bricks");
+        putToDict("minecraft:stone", "minecraft:cobblestone");
+        putToDict("minecraft:stone_slab", "minecraft:cobblestone_slab");
+        putToDict("minecraft:stone_brick_slab", "minecraft:cobblestone_brick_slab");
+        putToDict("minecraft:stone_pressure_plate", "minecraft:cobblestone_pressure_plate");
+        putToDict("minecraft:stone_button", "minecraft:cobblestone_button");
+        putToDict("minecraft:mossy_stone_bricks", "minecraft:mossy_cobblestone");
+        putToDict("minecraft:infested_mossy_stone_bricks", "minecraft:infested_mossy_cobblestone_bricks");
+        putToDict("minecraft:stone_brick_stairs", "minecraft:cobblestone_brick_stairs");
+        putToDict("minecraft:end_stone", "minecraft:end_cobblestone");
+        putToDict("minecraft:mossy_stone_brick_stairs", "minecraft:mossy_cobblestone_brick_stairs");
+        putToDict("minecraft:end_stone_brick_stairs", "minecraft:end_cobblestone_brick_stairs");
+        putToDict("minecraft:mossy_stone_brick_slab", "minecraft:mossy_cobblestone_brick_slab");
+        putToDict("minecraft:end_stone_brick_slab", "minecraft:end_cobblestone_brick_slab");
+        putToDict("minecraft:mossy_stone_brick_wall", "minecraft:mossy_cobblestone_brick_wall");
+        putToDict("minecraft:stone_brick_wall", "minecraft:cobblestone_brick_wall");
+        putToDict("minecraft:end_stone_brick_wall", "minecraft:end_cobblestone_brick_wall");
+        putToDict("minecraft:prismarine_bricks", "minecraft:prismarine");
+        putToDict("minecraft:prismarine_brick_stairs", "minecraft:prismarine_stairs");
+        putToDict("minecraft:cut_sandstone", "minecraft:sandstone");
+        putToDict("minecraft:mossy_cobblestone", "minecraft:gravel");
+    }
+}
