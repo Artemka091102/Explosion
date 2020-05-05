a8b19cad7214189d153ea4d8f0a106ce5e032371
Pugemon <pugemon03@gmail.com>
2020-05-05 19:29:53 +0300

    Added ExtraCobble Diorite

--- src/main/java/com/artemka091102/explosion/mods_integration/ExtraCobble.java
+++ src/main/java/com/artemka091102/explosion/mods_integration/ExtraCobble.java
@@ -6,20 +6,27 @@ public class ExtraCobble {
         SetupCrackedDict();
     }
     public static void SetupCrackedDict() {
+        //Andesite
         putToCrackedDict("minecraft:andesite", "extracobble:andesite_cobble");
         putToCrackedDict("minecraft:andesite_slab", "extracobble:andesite_cobble_slab");
         putToCrackedDict("minecraft:andesite_stairs", "extracobble:andesite_cobble_stairs");
-        putToCrackedDict("extracobble:mossy_andesite_cobble_wall", "extracobble:andesite_cobble_wall");
         putToCrackedDict("extracobble:andesite_cobble", "extracobble:andesite_gravel");
         putToCrackedDict("extracobble:andesite_gravel", "extracobble:andesite_sand");
-        putToCrackedDict("", "");
-        putToCrackedDict("", "");
-        putToCrackedDict("", "");
-        putToCrackedDict("", "");
-        putToCrackedDict("", "");
-        putToCrackedDict("", "");
-        putToCrackedDict("", "");
-        putToCrackedDict("", "");
+        putToCrackedDict("extracobble:mossy_andesite_cobble_wall", "extracobble:andesite_cobble_wall");
+        putToCrackedDict("extracobble:mossy_andesite_cobble", "extracobble:andesite_cobble");
+        putToCrackedDict("extracobble:mossy_andesite_cobble_slab", "extracobble:andesite_cobble_slab");
+        putToCrackedDict("extracobble:mossy_andesite_cobble_stairs", "extracobble:andesite_cobble_stairs");
+        //Diorite
+        putToCrackedDict("minecraft:diorite", "extracobble:diorite_cobble");
+        putToCrackedDict("minecraft:diorite_slab", "extracobble:diorite_cobble_slab");
+        putToCrackedDict("minecraft:diorite_stairs", "extracobble:diorite_cobble_stairs");
+        putToCrackedDict("extracobble:diorite_cobble", "extracobble:diorite_gravel");
+        putToCrackedDict("extracobble:diorite_gravel", "extracobble:diorite_sand");
+        putToCrackedDict("extracobble:mossy_diorite_cobble_wall", "extracobble:diorite_cobble_wall");
+        putToCrackedDict("extracobble:mossy_diorite_cobble", "extracobble:diorite_cobble");
+        putToCrackedDict("extracobble:mossy_diorite_cobble_slab", "extracobble:diorite_cobble_slab");
+        putToCrackedDict("extracobble:mossy_diorite_cobble_stairs", "extracobble:diorite_cobble_stairs");
+        //Granite
         putToCrackedDict("", "");
         putToCrackedDict("", "");
         putToCrackedDict("", "");
@@ -36,26 +43,26 @@ public class ExtraCobble {
 
 
 
-        /*extracobble:diorite_cobble
-        extracobble:diorite_cobble_slab
-        extracobble:diorite_cobble_stairs
-        extracobble:diorite_cobble_wall
-        extracobble:diorite_gravel
-        extracobble:diorite_sand
+        /*
+
+
+
+
+
         extracobble:granite_cobble
         extracobble:granite_cobble_slab
         extracobble:granite_cobble_stairs
         extracobble:granite_cobble_wall
         extracobble:granite_gravel
         extracobble:granite_sand
-        extracobble:mossy_andesite_cobble
-        extracobble:mossy_andesite_cobble_slab
-        extracobble:mossy_andesite_cobble_stairs
-        extracobble:mossy_andesite_cobble_wall
-        extracobble:mossy_diorite_cobble
-        extracobble:mossy_diorite_cobble_slab
-        extracobble:mossy_diorite_cobble_stairs
-        extracobble:mossy_diorite_cobble_wall
+
+
+
+
+
+
+
+
         extracobble:mossy_granite_cobble
         extracobble:mossy_granite_cobble_slab
         extracobble:mossy_granite_cobble_stairs
