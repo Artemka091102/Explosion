b788a6d2216860c94ee056cdb1e8fcb59d18be50
Pugemon <pugemon03@gmail.com>
2020-05-08 14:30:46 +0300

    Added New ExtraCobble blocks

--- src/main/java/com/artemka091102/explosion/mods_integration/ExtraCobble.java
+++ src/main/java/com/artemka091102/explosion/mods_integration/ExtraCobble.java
@@ -12,6 +12,9 @@ public class ExtraCobble {
         putToCrackedDict("minecraft:andesite_stairs", "extracobble:andesite_cobble_stairs");
         putToCrackedDict("extracobble:andesite_cobble", "extracobble:andesite_gravel");
         putToCrackedDict("extracobble:andesite_gravel", "extracobble:andesite_sand");
+        putToCrackedDict("extracobble:chiseled_andesite_bricks", "extracobble:andesite_bricks");
+        putToCrackedDict("extracobble:andesite_bricks", "extracobble:cracked_andesite_bricks");
+        putToCrackedDict("extracobble:cracked_andesite_bricks", "extracobble:andesite_cobble");
         //Mossy Andesite
         putToCrackedDict("extracobble:mossy_andesite_cobble", "extracobble:andesite_gravel");
         //Diorite
@@ -20,6 +23,9 @@ public class ExtraCobble {
         putToCrackedDict("minecraft:diorite_stairs", "extracobble:diorite_cobble_stairs");
         putToCrackedDict("extracobble:diorite_cobble", "extracobble:diorite_gravel");
         putToCrackedDict("extracobble:diorite_gravel", "extracobble:diorite_sand");
+        putToCrackedDict("extracobble:chiseled_diorite_bricks", "extracobble:diorite_bricks");
+        putToCrackedDict("extracobble:diorite_bricks", "extracobble:cracked_diorite_bricks");
+        putToCrackedDict("extracobble:cracked_diorite_bricks", "extracobble:diorite_cobble");
         //Mossy Diorite
         putToCrackedDict("extracobble:mossy_diorite_cobble", "extracobble:diorite_gravel");
         //Granite
@@ -28,7 +34,13 @@ public class ExtraCobble {
         putToCrackedDict("minecraft:granite_stairs", "extracobble:granite_cobble_stairs");
         putToCrackedDict("extracobble:granite_cobble", "extracobble:granite_gravel");
         putToCrackedDict("extracobble:granite_gravel", "extracobble:granite_sand");
+        putToCrackedDict("extracobble:chiseled_granite_bricks", "extracobble:granite_bricks");
+        putToCrackedDict("extracobble:diorite_bricks", "extracobble:cracked_diorite_bricks");
+        putToCrackedDict("extracobble:cracked_diorite_bricks", "extracobble:diorite_cobble");
         //Mossy Granite
         putToCrackedDict("extracobble:mossy_granite_cobble", "extracobble:granite_gravel");
+        //Stone
+        putToCrackedDict("minecraft:gravel", "extracobble:stone_sand");
+        putToCrackedDict("block.extracobble.stone_wall", "minecraft:cobblestone_brick_wall");
     }
 }
